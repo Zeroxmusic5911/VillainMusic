a4d6099296adf5ea13e6241ed2f5335b647a78d1
Zeroxmusic5911 <antispamods@gmail.com>
2024-11-22 00:06:30 +0530

    Update couples.py

--- SONALI/plugins/tools/couples.py
+++ SONALI/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SONALI.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="ꪜ 𝛊 ɭ ɭ ᧘ 𝛊 𝛈",
-            url=f"https://t.me/iamakki001",
+            text="𝐀 ‎ 𝐊 ‎ 𝐑 ‎ 🜲",
+            url=f"https://t.me/Atif_Khan_Rajput",
         ),
     ],
 ]
